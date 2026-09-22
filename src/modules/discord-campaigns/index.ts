e109430807e6c79736bee31163eec92e00ba06e1
Laiipj <160990405+futureproject971@users.noreply.github.com>
2026-09-22 11:29:41 -0300

    feat(discord): add campaign center API and models

---
+++ src/modules/discord-campaigns/index.ts
@@ -0,0 +1,11 @@
+export { DiscordCampaignCenterPage } from "./DiscordCampaignCenterPage";
+export { DiscordEmbedPreview } from "./DiscordEmbedPreview";
+export type {
+  DiscordCampaign,
+  DiscordCampaignCenterPayload,
+  DiscordCampaignStatus,
+  DiscordCampaignTarget,
+  DiscordCampaignTemplate,
+  DiscordCampaignWorker,
+  DiscordWorkerRole,
+} from "./types";
