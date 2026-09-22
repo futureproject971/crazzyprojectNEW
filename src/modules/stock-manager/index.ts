1c8511ff92386206410be17323e592262485979f
Laiipj <160990405+futureproject971@users.noreply.github.com>
2026-09-22 09:22:39 -0300

    feat(m27): export Stock Manager module

---
+++ src/modules/stock-manager/index.ts
@@ -0,0 +1,9 @@
+export { StockManagerPage } from "./StockManagerPage";
+export type {
+  StockPlanSummary,
+  StockBatchSummary,
+  StockEventSummary,
+  StockManagerCatalog,
+  StockManagerItem,
+  StockItemsResponse,
+} from "./types";
